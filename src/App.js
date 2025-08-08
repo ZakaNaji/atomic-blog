c3e9a62b9afd31b4c40e23b683c68d25f8f82116
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-08 16:40:24 +0100

    context provider pattern

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
-
-function createRandomPost() {
-  return {
-    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
-    body: faker.hacker.phrase(),
-  };
-}
-
-const PostsContext = createContext(null);
+import PostProvider, { PostsContext } from "./components/PostContext";
 
 function App() {
-  const [posts, setPosts] = useState(() =>
-    Array.from({ length: 30 }, () => createRandomPost())
-  );
-  const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
-
-  // Derived state. These are the posts that will actually be displayed
-  const searchedPosts =
-    searchQuery.length > 0
-      ? posts.filter((post) =>
-          `${post.title} ${post.body}`
-            .toLowerCase()
-            .includes(searchQuery.toLowerCase())
-        )
-      : posts;
-
-  function handleAddPost(post) {
-    setPosts((posts) => [post, ...posts]);
-  }
-
-  function handleClearPosts() {
-    setPosts([]);
-  }
-
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
     function () {
@@ -44,15 +13,7 @@ function App() {
   );
 
   return (
-    <PostsContext.Provider
-      value={{
-        searchedPosts,
-        handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-        handleAddPost,
-      }}
-    >
+    <PostProvider>
       <section>
         <button
           onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
@@ -66,7 +27,7 @@ function App() {
         <Archive />
         <Footer />
       </section>
-    </PostsContext.Provider>
+    </PostProvider>
   );
 }
 
@@ -201,3 +162,10 @@ function Footer() {
 }
 
 export default App;
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
