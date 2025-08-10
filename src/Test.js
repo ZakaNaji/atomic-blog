15340511d27fc81c5eba6e00f41b82f1d0ef13f2
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-10 12:05:38 +0100

    but if we pass an object this rule will be broken => each time state changes in Test the test object gets recreated => meaning that as memo is concerned the props changed.

--- src/Test.js
+++ src/Test.js
@@ -1,6 +1,6 @@
 import { memo, useMemo, useState } from "react";
 
-const SlowComponent = memo(function SlowComponent() {
+const SlowComponent = memo(function SlowComponent({ test }) {
   // If this is too slow on your maching, reduce the `length`
   const words = Array.from({ length: 100_000 }, () => "WORD");
   return (
@@ -16,11 +16,12 @@ const SlowComponent = memo(function SlowComponent() {
 
 export default function Test() {
   const [count, setCount] = useState(0);
+  const test = { test: false };
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      <SlowComponent test={test} />
     </div>
   );
 }
